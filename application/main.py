acaed80dbd0b54d4c50487db07a94154f9a838f2
Pavan <pavan.alpha.omega@gmail.com>
2025-01-27 00:04:55 +0000

    chore: added some example code into "main.py"

--- application/main.py
+++ application/main.py
@@ -2,9 +2,25 @@
 Contains the main code for this package.
 """
 
+# Import required libraries.
+import cv2 as cv
+
+# Import local modules.
+from imgprocess import direct_wells
+
+
 def main():
-    # TODO:Add functional code.
-    return
+    # TODO: Add functional code.
+    
+    ## Example/Placeholder code.
+    # Read in the test image.
+    image = cv.cvtColor(cv.imread("../tests/images/wells_96_0001.webp"), cv.COLOR_BGR2RGB)
+
+    # Get the RGB values at the well plate centres.
+    centres = direct_wells.get_well_centres(image)
+    colours = direct_wells.get_colours(image, centres)
+
+    return None
 
 
 if __name__ == "__main__":

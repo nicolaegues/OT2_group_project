9d95eb3002de40f188914eac8dfda6ca84b06a9c
sebparkin <sebastianparkin@gmail.com>
2025-03-06 19:48:44 +0000

    added the reading of a file and made it so it works with the simulation

--- application/opentrons_script.py
+++ application/opentrons_script.py
@@ -5,13 +5,14 @@ import numpy as np
 
 
 #creates array with volumes, in future this will come from the optimisation program
-volume = np.array([[20.0, 40.0, 60.0] for i in range(12)])
+#volume = np.array([[20.0, 30.0, 40.0] for i in range(14)])
+iter_n = int(open('iter_count.txt').read())
+volume = np.load('values.npy')
 
 requirements = {"robotType": "OT-2", "apiLevel": "2.16"}
 
 #need to have some way for the user to select labware type and number
 well_loc = 5
-iter_n = 0
 
 #concentrations used must come in a num of wells x num of liquids size array
 iter_size = volume.shape[0]
@@ -32,7 +33,8 @@ def run(protocol: protocol_api.ProtocolContext):
     reservoir = protocol.load_labware("nest_12_reservoir_15ml", 2)
     #well plate in the middle for optimal camera placement, in future let user select
     plate = protocol.load_labware("nest_96_wellplate_200ul_flat", well_loc)
-    left_pipette = protocol.load_instrument("p300_multi_gen2", "right", tip_racks=[tips])
+    left_pipette = protocol.load_instrument("p300_single_gen2", "right", tip_racks=[tips])
+
 
     row = plate.rows()
     for i in range(num_liquids):
@@ -41,6 +43,7 @@ def run(protocol: protocol_api.ProtocolContext):
             #calculates current column and row to pipette into
             current_col = start_col + j % 12
             current_row = start_row + (start_col + j) // 12
+
             #transfers X amount of liquid i into the well
             if i != num_liquids - 1:
                 left_pipette.transfer(amt[j], reservoir[f'A{i+1}'], row[current_row][current_col])

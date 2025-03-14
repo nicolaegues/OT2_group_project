522cf7e86cd3e674b03d0883040bc8a8f15e6d6c
Nicola Egües <nicola.egues@gmail.com>
2025-03-14 07:28:11 +0000

    cleaned up the code, and made various edits

--- application/main.py
+++ application/main.py
@@ -1,40 +1,60 @@
 ##main script
 import opt_functions
-from wellplate_classes import wellplate96
+from wellplate_classes_sugg import wellplate96
 import numpy as np
+import datetime
+import os
+pass
+
+
+#Tell the user where to place the liquids in the reservoir!! for correct indexing
+#allow user to tweak optim params for algorithms here
+
 
 def main():
 
-    liquid_names = ['blue', 'green', 'red']
-    search_space = [[0.0, 150/3], [0.0, 150/3], [0.0, 150/3]]
-    iteration_size = 12
-    iteration_number = 8
-    if iteration_size * iteration_number > 96:
-        print('Error: too many iterations')
+    #these are primarly for later data storage folder/file naming
+    optimisation_id = "PSO"
+    exp_no = "0"
+
+    current_datetime = datetime.datetime.now().strftime("%a-%d-%b-%Y-at-%I-%M-%S%p")
+    exp_id = f"exp_{exp_no}_{optimisation_id}_{current_datetime}" 
+    data_storage_folder = "data" 
+    exp_data_dir = f"{data_storage_folder}/{exp_id}"
+    os.makedirs(exp_data_dir, exist_ok=True)
+
+    wellplate_size = 96 
+
+    wellplate_shape = (8, 12) #as viewed horizontally
+    wells_per_iteration = 12
+    max_iterations = 8
+
+    if wells_per_iteration * max_iterations > wellplate_size:
+        print('Error: too many iterations') 
         return None
-
-    #example function to be minimized for colours, sums it then squares it
-    #we need to import and add the camera recognition function here, can be created as a class with
-    #__call__ to allow it to use parameters but still only input params and return values.
-    #instead we could have more variables passed into it, and just write in the docs which ones
-    #it would be easier to pass start_index and iter_size or iter_n and iter_size to locate the correct section of wells
-    def auto_input(params):
-        ideal = np.array([30.0, 20.0, 12.0])
-        values = ((params - ideal)**2).sum(axis = 1)
-        return values
     
-    #import and create image capture and colour scanning function here. If it outputs a 96 long array we can adjust how the
-    #function is called so that indexing is easy within the function
 
-    #type 'Manual' in the function box to allow manual input
-    model = wellplate96(auto_input, iteration_size, liquid_names)
+    liquid_names = ['blue', 'yellow', 'red', "water"] #Tell the user where to place these liquids in the reservoir!!
+    measurement_parameter_names = ["measured_red", "measured_green", "measured_blue"]
+    manual_measurement = True
 
-    #allow user to tweak params for algorithms here
+    ideal_measurement = [30.0, 30.0, 30.0] # our ideal RGB value
+    search_space = [[0.0, 150/3], [0.0, 150/3], [0.0, 150/3]]
+
+    well_locs = [5, 8]
+    total_volume = 150.0
+
+    def objective_function(measurements):  
+        errors = ((measurements - ideal_measurement)**2).sum(axis = 1)
+        return errors
+
+    model = wellplate96(objective_function, exp_data_dir, wellplate_shape, wells_per_iteration, manual_measurement, liquid_names, measurement_parameter_names, well_locs, total_volume)
 
     #call particle_swarm, random_forest, or gaussian process
-    opt_functions.particle_swarm(model, search_space, iteration_number)
+    if optimisation_id == "PSO":
+        opt_functions.particle_swarm(model, search_space, max_iterations, wells_per_iteration)
+    
 
 
-
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+main()

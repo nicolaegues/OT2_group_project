ed209e17788d9a96904255199f974371b050f39c
Nicola Egües <nicola.egues@gmail.com>
2025-03-14 17:27:46 +0000

    fixed mixing at the end (and tip-picking up and dropping), and changed wellplate and tiprack

--- application/opentrons_script_generator.py
+++ application/opentrons_script_generator.py
@@ -3,6 +3,14 @@ import sys
 from opentrons import protocol_api
 import numpy as np
 
+"""
+- need to think about whether we want a deeper wellplate for better mixing of the colors. I have a picture of the alterantive - only issue is that it is squared. 
+- how do we do it such that for red, for example, it doesn't go back to the reservoir every time for each well but gathers multiple well volumes at once?
+- user input options for labware and positioning (in main). 
+- comment all codes properly
+
+"""
+
 
 def generate_script(filepath, iter_count, wells_per_iteration, liquid_volumes, well_locs):
     '''
@@ -44,14 +52,14 @@ def run(protocol: protocol_api.ProtocolContext):
 
     if 1 not in protocol.deck or protocol.deck[1] is None:
         #loading the tips, reservoir and well plate into the program
-        tips = protocol.load_labware("opentrons_96_tiprack_300ul", 1)
+        tips = protocol.load_labware("opentrons_96_tiprack_1000ul", 1)
         reservoir = protocol.load_labware("nest_12_reservoir_15ml", 2)
         
         plates = {{}}
         for idx, loc in enumerate(well_locs):
-            plates[f"plate_{{idx+1}}"] = protocol.load_labware("nest_96_wellplate_200ul_flat", loc)
+            plates[f"plate_{{idx+1}}"] = protocol.load_labware("nest_96_wellplate_100ul_pcr_full_skirt", loc)
         
-        left_pipette = protocol.load_instrument("p300_single_gen2", "right", tip_racks=[tips])
+        left_pipette = protocol.load_instrument("p1000_single_gen2", "right", tip_racks=[tips])
     
     else:
         #retrieve existing labware
@@ -67,11 +75,11 @@ def run(protocol: protocol_api.ProtocolContext):
 
     well_count = iteration_count - current_plate_idx*8
 
-    target_wells = []
     for liquid in range(num_liquids): 
 
         left_pipette.pick_up_tip() #one tip for each dye-distribution into all the wells. then a new tip for another color distribution into all the wells. 
 
+        target_wells = []
         for well, volume_set in enumerate(volumes):
 
             #multiplying by factor of 8: this way we first fill A1 - A12, then B1-B12. instead of A1-H1, then A2-H2.... 
@@ -90,11 +98,10 @@ def run(protocol: protocol_api.ProtocolContext):
         #bin the tip
         left_pipette.drop_tip()
 
-        
-    left_pipette.pick_up_tip()
     for well in target_wells:
+        left_pipette.pick_up_tip()
         left_pipette.mix(3, 20, well)
-    left_pipette.drop_tip()
+        left_pipette.drop_tip()
                 
 
 '''
